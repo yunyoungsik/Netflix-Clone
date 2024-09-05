7161b9baeff42e0a05ed975b0d2440c0f860ec5f
yunyoungsik <yunyoungsik91@gmail.com>
2024-09-05 23:32:13 +0900

    [20240905]5:14:02 시작

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
+import WatchPage from './pages/WatchPage';
 
 // components
 import Footer from './components/Footer';
@@ -35,6 +36,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={!user ? <LoginPage /> : <Navigate to={'/'} />} />
         <Route path="/signup" element={!user ? <SignUpPage /> : <Navigate to={'/'} />} />
+        <Route path="/watch/:id" element={!user ? <WatchPage /> : <Navigate to={'/'} />} />
       </Routes>
       <Footer />
 
