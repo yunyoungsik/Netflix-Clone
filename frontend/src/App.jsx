031c33d6f9487d42c0326cf8c59c66acdac37927
yunyoungsik <yunyoungsik91@gmail.com>
2024-09-06 00:42:42 +0900

    [20240906]5:49:16까지 완료

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={!user ? <LoginPage /> : <Navigate to={'/'} />} />
         <Route path="/signup" element={!user ? <SignUpPage /> : <Navigate to={'/'} />} />
-        <Route path="/watch/:id" element={!user ? <WatchPage /> : <Navigate to={'/'} />} />
+        <Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
       </Routes>
       <Footer />
 
